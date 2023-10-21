f91ff5409a96c6c521094a8ae920bc8bf7d0b6f4
Tog <tog.gavk@gmail.com>
2023-10-21 21:11:18 +0300

    Started creation of architecture

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { mod } = require("numjs");
 const pdf = require("pdf-parse");
 
 const filePath = "./files/text.pdf";
@@ -24,7 +25,7 @@ async function processPdf(filePath, language) {
     await processPdf(filePath2, "english");
     console.log(trainingData);
 
-    const detector = new LanguageDetector(trainingData);
+    const detector = new FrequentWordsModel(trainingData);
 
     const textToDetect =
         "Мы строим города и разводим мосты, ткачи судеб и создатели своей собственной судьбы.";
@@ -33,33 +34,49 @@ async function processPdf(filePath, language) {
     console.log(scores);
 })();
 
-class LanguageDetector {
+// Получить словарь типа:
+// {
+//      язык: [тренировочный_текст1, тренировочный_текст2, ..., тренировочный_текстn]
+// }
+let getTrainingData = () => {
+    // Надо реализовать...
+}
+
+// Загрузить json из файла 'filename'
+let loadFromFile = (filename) => {
+    // Надо реализовать...
+}
+
+// Сохранить json переданный в параметре data в файл с именем 'filename'
+let saveToFile = (filename, data) => {
+    // Надо реализовать
+}
+
+class FrequentWordsModel {
     constructor(trainingData) {
         this.languageModels = {};
-
-        for (let language in trainingData) {
-            this.languageModels[language] = this.train(trainingData[language]);
-        }
     }
 
-    train(data) {
-        const wordCounts = {};
+    train(trainingData) {
+        for (let language in trainingData) {
+            const wordCounts = {};
 
-        data.forEach((text) => {
-            const words = text.split(" ");
-            words.forEach((word) => {
-                if (!wordCounts[word]) {
-                    wordCounts[word] = 0;
-                }
-                wordCounts[word]++;
+            data.forEach((text) => {
+                const words = text.split(" ");
+                words.forEach((word) => {
+                    if (!wordCounts[word]) {
+                        wordCounts[word] = 0;
+                    }
+                    wordCounts[word]++;
+                });
             });
-        });
 
-        const sortedWords = Object.keys(wordCounts).sort(
-            (a, b) => wordCounts[b] - wordCounts[a]
-        );
+            const sortedWords = Object.keys(wordCounts).sort(
+                (a, b) => wordCounts[b] - wordCounts[a]
+            );
 
-        return sortedWords.slice(0, 100); // ПОЯ - первые 100 слов с наибольшей частотой
+            this.languageModels[language] = sortedWords.slice(0, 100); // ПОЯ - первые 100 слов с наибольшей частотой
+        } 
     }
 
     detect(text) {
@@ -81,4 +98,44 @@ class LanguageDetector {
 
         return scores;
     }
+
+    save(filename) {
+        return false;
+    }
+
+    load(filename) {
+        return false;
+    }
+}
+
+// Определить язык методом ЧАСТОТНЫХ СЛОВ
+// documents - текста, для которых происходить определение языка
+let detectLanguageFW = (document) => {
+    const modelFilename = "fwmodel.json"
+    let model = FrequentWordsModel();
+    
+    if (! model.load(modelFilename)) {
+        model.train(getTrainingData());
+        model.save(modelFilename);
+    }
+
+    let evaluation = model.detect(document);
+
+    let result = null;
+    for (let language in evaluation) {
+        if (result == null) {
+            result = language;
+            continue;
+        }
+
+        if (evaluation[language] > evaluation[result]) {
+            result = language;
+        }
+    }
+
+    return {
+        language: result,
+        probability: evaluation[result],
+        text: document
+    };
 }
