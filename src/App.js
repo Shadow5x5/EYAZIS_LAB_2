cc3aaa7942e875525bdbb195f39a48890d4780bc
Shadow5x5 <Shadow77x77@gmail.com>
2023-11-04 16:09:08 +0300

    client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedFile, setSelectedFile] = useState(null);
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+    const handleUpload = async () => {
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append("file", selectedFile);
+
+            try {
+                const response = await axios.post(
+                    "http://localhost:5000/",
+                    formData,
+                    {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
+                    }
+                );
+
+                console.log("File uploaded successfully", response);
+            } catch (error) {
+                console.error("Error uploading file", error);
+            }
+        } else {
+            console.error("Please select a file before uploading.");
+        }
+    };
+    return (
+        <div className="App">
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Отправить</button>
+            <p></p>
+        </div>
+    );
 }
 
 export default App;
